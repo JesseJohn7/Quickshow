d04be46a789f1c92690d23ad9bc38185d4ed109b
JesseJohn7 <jessejohn260@gmail.com>
2025-12-16 10:58:58 +0100

    added blur circle

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import { ArrowRight } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+import BlurCircle from './BlurCircle';
+
 const FeaturedSection = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
-      <div>
-        <p>Now Showing</p>
-        <button className='group-flex items-center gap-2 text-sm  text-gray-300'>
+      <BlurCircle/>
+      <div className='relative flex items-center justify-between pt-20 pb-10'>
+        <p className='text-gray-300 font-medium text-lg'>Now Showing</p>
+        
+        <button
+          onClick={() => navigate('/movies')}
+          className='group flex items-center gap-2 text-sm text-gray-300 hover:text-white transition'
+        >
           View All
-          <ArrowRight className='group-hover:translate-x-0.5 transition w-4.5 h-4.5'/>
-          </button>
+          <ArrowRight className='group-hover:translate-x-0.5 transition w-4 h-4' />
+        </button>
       </div>
+
       <div></div>
       <div></div>
-      </div>
+    </div>
   )
 }
 
